9b83ad54e588518ffbf816f8af0c524b439d9091
张新兴 <zhangxinxing1@xiaomi.com>
2017-04-20 18:07:26 +0800

    init pro

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,15 +3,30 @@ require('styles/App.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+// 利用函数将图片名信息转成图片URL路径信息
+let imageDatas = require('../data/imageDatas.json');
+
+imageDatas = (function getImageURL(imageDataArr){
+	for (var i=0, j=imageDataArr.length; i<j; i++){
+		var singleImageData = imageDataArr[i];
+
+		singleImageData.imageURL = require('../images'+singleImageData.fileName);
+
+		imageDataArr[i] = singleImageData;
+	}
+
+	return imageDataArr;
+})(imageDatas);
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      	<section className="stage">
+      		<section className="img-sec">
+      		</section>
+      		<nav className="controller-nav">
+      		</nav>
+      	</section>
     );
   }
 }
